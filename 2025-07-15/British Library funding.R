d98e7d1fed490169ca862258e687eecf49d67373
Lyubomir N. Kolev <lyubomirnkolevii@gmail.com>
2025-07-15 13:26:03 -0400

    Cleaning up wiht geom_textline

--- 2025-07-15/British Library funding.R
+++ 2025-07-15/British Library funding.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(geomtextpath)
 
 funding <-
   read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-07-15/bl_funding.csv")
@@ -6,17 +7,36 @@ funding <-
 data_dict <-
   read_tsv("Data dictionary.txt")
 
+funding_non_adjusted <-
+  funding |> 
+  select(
+    year,
+    nominal_gbp_millions:other_gbp_millions
+  ) |> 
+  pivot_longer(
+    nominal_gbp_millions:other_gbp_millions,
+    names_to = "Funding type",
+    values_to = "GBP"
+  ) |>
+  distinct() |> 
+  group_by(
+    year,
+    `Funding type`
+  ) |> 
+  summarize(
+    "Total GBP" = sum(GBP),
+    .groups = "drop"
+  )
+
 overall_graph <-
   ggplot(
-    data = funding,
-    aes(x = year)
+    data = funding_non_adjusted,
+    aes(x = year, y = `Total GBP`, color = `Funding type`, label = `Funding type`)
   ) +
-  geom_line(aes(y = nominal_gbp_millions, color = nominal_gbp_millions)) +
-  geom_line(aes(y = gia_gbp_millions, color = gia_gbp_millions)) +
-  geom_line(aes(y = voluntary_gbp_millions, color = voluntary_gbp_millions)) +
-  geom_line(aes(y = investment_gbp_millions, color = investment_gbp_millions)) +
-  geom_line(aes(y = services_gbp_millions, color = services_gbp_millions)) +
-  geom_line(aes(y = other_gbp_millions, color = other_gbp_millions)) +
-  scale_color_manual(name = "Revenue", values = c("red", "green", "blue", "orange", "yellow", "purle")) +
-  theme_minimal()
+  geom_line() +
+  geom_textline(linewidth = 2) +
+  theme_minimal() +
+  theme(
+    legend.position = "none"
+  )
 overall_graph
