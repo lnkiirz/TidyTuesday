7180ecca275f998cff5e92612eac26e527e83021
Lyubomir N. Kolev <lyubomirnkolevii@gmail.com>
2025-07-29 11:24:56 -0400

    Deleting old file

--- 2025-07-29/Netflix shows an dmovies.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-
-movies <- 
-  read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-07-29/movies.csv')
-
-shows <- 
-  read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-07-29/shows.csv')
